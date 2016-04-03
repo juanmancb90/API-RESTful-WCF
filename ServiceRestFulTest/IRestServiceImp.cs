cdd1aeb5b4e1f49b1ca6fa9aa1c2fe9f806fe518
juanmanuel chaguendo bermeo <juanman90@gmail.com>
2016-04-03 16:27:18 -0500

    upload web service RESTful

---
+++ ServiceRestFulTest/IRestServiceImp.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace ServiceRestFulTest
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IRestServiceImp" in both code and config file together.
+    [ServiceContract]
+    public interface IRestServiceImp
+    {
+        [OperationContract]
+        void DoWork();
+
+        //xml RESTful api 
+        [OperationContract]
+        [WebInvoke(Method = "GET",
+            ResponseFormat = WebMessageFormat.Xml,
+            BodyStyle = WebMessageBodyStyle.Wrapped,
+            UriTemplate = "xml/{id}")]
+        string dataXML(string id);
+
+        //json RESTful api
+        [OperationContract]
+        [WebInvoke(Method = "GET",
+            ResponseFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Wrapped,
+            UriTemplate = "json/{id}")]
+        string dataJSON(string id);
+    }
+
+    
+}
---
+++ ServiceRestFulTest/RestServiceImp.svc.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ServiceRestFulTest
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "RestServiceImp" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select RestServiceImp.svc or RestServiceImp.svc.cs at the Solution Explorer and start debugging.
+    public class RestServiceImp : IRestServiceImp
+    {
+        public void DoWork()
+        {
+        }
+
+        public string dataXML(string id) 
+        {
+            return "Request id: " + id;
+        }
+
+        public string dataJSON(string id) 
+        {
+            return "request id: " + id;
+        }
+    }
+}
